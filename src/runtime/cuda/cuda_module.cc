cae1ad48dbe126101ddb6292940b11149fda019b
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2019-01-21 16:09:49 -0800

    [Recogni] Add dump of grid and thread dims on kernel launch

--- src/runtime/cuda/cuda_module.cc
+++ src/runtime/cuda/cuda_module.cc
@@ -167,6 +167,15 @@ class CUDAWrappedFunc {
     }
     CUstream strm = static_cast<CUstream>(CUDAThreadEntry::ThreadLocal()->stream);
     ThreadWorkLoad wl = thread_axis_cfg_.Extract(args);
+
+    {
+      std::ostringstream os;
+      os << "Launching kernel grid=(" << wl.grid_dim(0) << ","
+         << wl.grid_dim(1) << "," << wl.grid_dim(2) << "), "
+         << " block=(" << wl.block_dim(0) << ","
+         << wl.block_dim(1) << "," << wl.block_dim(2) << ")\n";
+      LOG(INFO) << os.str();
+    }
     CUresult result = cuLaunchKernel(
         fcache_[device_id],
         wl.grid_dim(0),
