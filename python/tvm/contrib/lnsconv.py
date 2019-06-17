95778f8d8ae51d1949d2edbb48cadbcd50bb1eda
Eugene Feinberg <eugene@recogni.com>
2019-06-17 13:18:09 -0700

    Example

--- python/tvm/contrib/lnsconv.py
+++ python/tvm/contrib/lnsconv.py
@@ -6,6 +6,6 @@ from .. import intrin as _intrin
 from .._ffi.function import _init_api
 
 def conv3x3(data,weights):
-    return _api.extern((1), [data, weights], 
+    return _api.extern( (data.shape[0]), [data, weights], 
         lambda ins, outs: _intrin.call_packed("tvm.contrib.lnsconv.conv3x3", ins[0], ins[1], outs[0]), dtype='float32')
 
--- tests/python/contrib/test_lnsconv.py
+++ tests/python/contrib/test_lnsconv.py
@@ -7,20 +7,41 @@ def test_conv():
 
     ctx = tvm.gpu(0)
 
-    A = tvm.placeholder((3,3), name="data")
-    B = tvm.placeholder((3,3), name="weights")
-    C = lnsconv.conv3x3(A,B)
-    D = tvm.compute(C.shape, lambda i: C(i), name='D')
-    s = tvm.create_schedule(D.op)
+    # Number of channels
+    n = tvm.var('n')
+
+    A = tvm.placeholder((n,3,3), name="data")
+    B = tvm.placeholder((n,3,3), name="weights")
+    rx = tvm.reduce_axis((0,3))
+    ry = tvm.reduce_axis((0,3))
+
+    As = A[n,rx,ry]
+    Bs = B[n,rx,ry]
+
+    D = lnsconv.conv3x3(A,B)
+    
+    E = tvm.compute( (n,0,0), lambda x: D[x], name='E')
+
+    s = tvm.create_schedule(E.op)
    
     block_y = tvm.thread_axis("blockIdx.y")
-   
-    s[D].bind(D.op.axis[0], block_y)
-    s[D].storage_align(block_y, 64, 0)
+    
+    s[E].bind(E.op.axis[0], block_y)
+    s[E].storage_align(block_y, 64, 0)
 
-    f = tvm.build(s, [A,B,D], "cuda", target_host="llvm", name="function_wrapper")
+    # Show IR 
+    print('-------------------- IR -----------------------')
+    print(tvm.lower(s,[A,B,E],simple_mode=True)) 
+
+    f = tvm.build(s, [A,B,E], "cuda", target_host="llvm", name="function_wrapper")
+    
+    # Show cuda
+    print('-------------------- CUDA -----------------------')
     print(f.imported_modules[0].get_source())
 
+    # Dump out generated code
+    f.export_library("foo.so")
+    f.save("foo.o")
 
 if __name__ == "__main__":
     test_conv()
